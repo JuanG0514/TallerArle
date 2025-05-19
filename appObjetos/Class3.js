2558c2b1f6d7edda930d6cd9540d7936cbfe7143
Juan Gomez <juandavidgome336@gmail.com>
2025-05-19 09:03:16 -0500

    posible solucion

--- appObjetos/Class3.js
+++ appObjetos/Class3.js
@@ -34,3 +34,46 @@ class Cliente{
 const cliente = new Cliente();
 
 cliente.agregarCliente("juan")
+
+
+// class Cliente {
+//     constructor(nombre, turno) {
+//         this.nombre = nombre;
+//         this.turno = turno;
+//     }
+// }
+
+// class ColaClientes {
+//     constructor() {
+//         this.clientes = [];
+//         this.turnoActual = 1;
+//     }
+
+//     agregarCliente(nombre) {
+//         if (nombre != null && nombre !== "") {
+//             const nuevoCliente = new Cliente(nombre, this.turnoActual++);
+//             this.clientes.push(nuevoCliente);
+//             console.log(`El cliente ${nuevoCliente.nombre} se añadió con el turno ${nuevoCliente.turno}`);
+//         } else {
+//             console.log("No hay personas en la cola");
+//         }
+//     }
+
+//     verCola() {
+//         if (!this.clientes || this.clientes.length === 0) {
+//             console.log("No hay personas por ver en la cola.");
+//         } else {
+//             let lista = "";
+//             this.clientes.forEach((cliente, index) => {
+//                 lista += `${index + 1}. ${cliente.nombre} - turno: ${cliente.turno}\n`;
+//             });
+//             console.log("Personas en la cola:\n" + lista);
+//         }
+//     }
+// }
+
+// // Ejemplo de uso:
+// const cola = new ColaClientes();
+// cola.agregarCliente("Juan");
+// cola.agregarCliente("Ana");
+// cola.verCola();
